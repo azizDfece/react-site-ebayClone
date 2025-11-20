ea3f658f4f60139640fcf738ff16a5cf0014827f
Abdulaziz <anewaziz1@gmail.com>
2025-11-21 00:42:26 +0300

    finish header

--- src/component/header.jsx
+++ src/component/header.jsx
@@ -8,6 +8,8 @@ import { useEffect, useRef } from 'react'
 function Header() {
     const fDdHeader = useRef(null)
     const watchList = useRef(null)
+    const myebay = useRef(null)
+    const thd2 = useRef(null)
 
     useEffect(() => {
         const trigger = watchList.current
@@ -39,6 +41,32 @@ function Header() {
         }
 
     }, [])
+    useEffect(() => {
+        const trigger = myebay.current
+        const dropdown2 = thd2.current
+
+        if (!trigger) return
+
+        function handledisplaydropdown() {
+            dropdown2.style.display = 'block'
+        }
+        function handleClickoutsie(e) {
+
+            if (dropdown2 && !dropdown2.contains(e.target)
+                && !trigger.contains(e.target)) {
+                dropdown2.style.display = 'none'
+            }
+
+        }
+
+        trigger.addEventListener("mouseover", handledisplaydropdown)
+        document.addEventListener('click', handleClickoutsie)
+
+        return () => {
+            trigger.removeEventListener("mouseover", handledisplaydropdown)
+            document.removeEventListener("click", handleClickoutsie)
+        }
+    }, [])
 
     return (
 
@@ -60,7 +88,14 @@ function Header() {
                     <div className='top-header-dropdown' ref={watchList}>Watchlist  ^
                         <div className='t-h-d' ref={fDdHeader}>لا يوجد اشياء</div>
                     </div>
-                    <p className='top-header-dropdown2'>My ebay  ^</p>
+                    <div className='top-header-dropdown2' ref={myebay}>My ebay  ^
+                        <div className="t-h-d2" ref={thd2}>
+                            <p>Summary</p>
+                            <p>Recent viwed</p>
+                            <p>Recent viwed</p>
+                            <p>Bids/Offers</p>
+                        </div>
+                    </div>
                 </div>
             </div>
 
@@ -68,14 +103,18 @@ function Header() {
                 <div className="first-elements">
                     <img src="https://unsplash.it/100/100" alt="" className="logo" />
                     <select name="" id="" className="dropdown">
-                        <option value="">تسوق حسب التصنيف</option>
+                        <option value="">تسوق حسب تصنيف</option>
                     </select>
                 </div>
                 <div className="midle-element">
-                    <input type="text" />
+                    <input type="text" placeholder='Search for anything' />
                     <select name="" id="" className="search-input-dropdown">
                         <option value="">All Catgory</option>
                     </select>
+                </div>
+                <div className="last-element">
+                    <button className="praimy-btn">Search</button>
+                    <a href="#">Advanced</a>
                 </div>
             </div>
         </header>
